ad81d6acecd55efc08e7bd34d4efab9fad08d6fd
Joey Lin <joelinkaiyi@gmail.com>
2026-01-08 22:26:15 +0800

    backend deploy setting

--- backend/main.go
+++ backend/main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"nkust-calendar/handlers"
-	"time"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"nkust-calendar/handlers"
+	"os"
 )
 
 func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"}, 
-		AllowMethods:     []string{"POST", "OPTIONS"},
-		AllowHeaders:     []string{"Content-Type"},
-		ExposeHeaders:    []string{"Content-Disposition"}, 
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
+    AllowOrigins:     []string{"https://nkust-cal-convert.vercel.app/"}, 
+    AllowMethods:     []string{"POST", "OPTIONS"},
+    AllowHeaders:     []string{"Content-Type"},
+    ExposeHeaders:    []string{"Content-Disposition"},
+    AllowCredentials: true,
+}))
 
 	// 設定 API 路由
 	r.POST("/api/export-ics", handlers.ExportICSHandler)
-
-	r.Run(":8081")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8081"
+	}
+	r.Run(":" + port)
 }
