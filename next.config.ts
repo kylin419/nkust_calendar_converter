7944831d3b9a4a7b27c4c7b63426d953c66d768a
Joey Lin <joelinkaiyi@gmail.com>
2026-01-09 01:30:34 +0800

    fix deploy issue by rewrite next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    // 忽略伺服器端不需要的二進制依賴
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
 };
 
 export default nextConfig;
